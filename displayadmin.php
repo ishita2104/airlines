362a6b3768797d80081470229daffe999fd41884
ishita2104 <60072783+ishita2104@users.noreply.github.com>
2021-01-16 20:34:47 +0530

    Create displayadmin.php

---
+++ displayadmin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOMELOGIN</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+		body
+		{
+			background-color: #333333;
+		}
+	</style>
+</head>
+<body>
+	&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+	<a href="insertflight.php"><button class="my-button" style="width: 300px; text-align: center;
+	height: 80px;
+	font-size: 30px;
+	background-color: black;
+	color: white;
+	border:2px solid white;
+	margin-top: 200px;
+	margin-left: 15%;
+	box-shadow: 2px 2px white;
+	border-radius: 9px;">ADD FLIGHT DETAILS</button></a>
+	<br><br><br>
+	&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+	<a href="adminview.php"><button class="my-button1" style="width: 300px;
+	height: 60px;
+	font-size: 30px;
+	background-color: black;
+	color: white;
+	margin-left: 15%;
+	border:2px solid white;
+	box-shadow: 2px 2px white;
+	border-radius: 9px;">VIEW USER</button></a>
+	
+
+</body>
+</html>
