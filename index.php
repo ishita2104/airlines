b7c57b42eaece4b291614f7871dc9b52cc35e214
ishita2104 <60072783+ishita2104@users.noreply.github.com>
2021-01-16 20:38:11 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		AIRLINES MANAGEMENT SYSTEM
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<style type="text/css">
+	nav
+	{
+	    float: right;
+	    word-spacing: 20px;
+	    padding: 20px;
+	}
+	nav li
+	{
+	    display: inline-block;
+	    line-height: 80px;
+	}
+</style>
+<body>
+	<div class="wraper">
+		
+	
+		<header>
+			<div class="logo">
+				<img src="images/2.jpeg">									
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">HOME</a></li>
+					<li><a href="homelogin.php">LOGIN</a></li>
+					<li><a href="flight.php">FLIGHT_DETAILS</a></li>
+					<li><a href="contact.php">CONTACT</a></li>
+					
+				</ul>
+				
+			</nav>
+			<marquee style="color: white;">FLIGHTS HAVE BEEN CANCELLED DUE TO RAIN. WE WILL ANNOUNCE THE DETAILS SHORTLY. STAY HOME STAY SAFE</marquee>
+		</header>
+		<section>
+			<div class="sec_img">
+				
+				
+				<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+				<div class="box">
+					<br>
+					<h1 style="color: white;text-align: center;font-size: 25px;">WELCOME TO ABC AIRLINES</h1>
+				</div>
+			</div>
+			
+		</section>
+		<footer>
+			<p style="color: white;float:right;">
+				Email: ABC_airlines@gmail.com<br>
+				Mobile: 1234567890
+			</p>
+			
+		</footer>
+	</div>
+</body>
+</html>
