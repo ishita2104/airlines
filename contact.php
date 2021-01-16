121594e14fb2cd530fbbeb2f6e706a9665efa3d7
ishita2104 <60072783+ishita2104@users.noreply.github.com>
2021-01-16 20:34:13 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		CONTACT
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<style type="text/css">
+	nav
+	{
+	    float: right;
+	    word-spacing: 20px;
+	    padding: 20px;
+	}
+	nav li
+	{
+	    display: inline-block;
+	    line-height: 80px;
+	}
+</style>
+<body>
+	<div class="wraper">
+		
+	
+		<header>
+			<div class="logo">
+				<img src="images/2.jpeg">									
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">HOME</a></li>
+					<li><a href="homelogin.php">LOGIN</a></li>
+					<li><a href="flight.php">FLIGHT_DETAILS</a></li>
+					<li><a href="">CONTACT</a></li>
+					
+				</ul>
+				
+			</nav>
+		</header>
+		<section style="background-color:  #333333;height: 530px;">
+			<br><br><br><br><br><br><br><br>
+			<h1 style="color: white;text-align: center;"><a href="mailto:ABC_airlines@gmail.com" style="color: white;font-size: 32px;text-decoration: none;">EMAIL US: &nbsp; ABC_airlines@gmail.com</a></h1>
+			<br><br><br><br><br>
+			<h1 style="color: white;text-align: center;font-size: 32px;">Mobile: 1234567890</h1>
+				
+				
+			</h1>
+		</section>
