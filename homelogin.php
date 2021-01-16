80f1f03fe3a00eb76d1f7ceabcaa37fb8f40f66b
ishita2104 <60072783+ishita2104@users.noreply.github.com>
2021-01-16 20:37:12 +0530

    Create homelogin.php

---
+++ homelogin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOMELOGIN</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+		body
+		{
+			background-color: #333333;
+		}
+	</style>
+</head>
+<body>
+	&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+	<a href="admin.php"><button class="my-button" style="width: 200px;
+	height: 55px;
+	font-size: 30px;
+	background-color: black;
+	color: white;
+	border:2px solid white;
+	margin-top: 250px;
+	box-shadow: 2px 2px white;
+	border-radius: 9px;">ADMIN</button></a>
+	&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+	<a href="login.php"><button class="my-button1" style="width: 200px;
+	height: 55px;
+	font-size: 30px;
+	background-color: black;
+	color: white;
+	border:2px solid white;
+	box-shadow: 2px 2px white;
+	border-radius: 9px;">USER</button></a>
+	
+
+</body>
+</html>
