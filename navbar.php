4a012880c090441c3afb6e22f283cfd7df56f36d
ishita2104 <60072783+ishita2104@users.noreply.github.com>
2021-01-16 20:41:10 +0530

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<!--<img src="images/2.jpeg">!-->									
+				</div>
+				
+				<ul class="nav navbar-nav">	
+					<li><a href="index.php">HOME</a></li>						
+					<li><a href="flight.php">FLIGHT_DETAILS</a></li>	
+					<li><a href="contact.php">CONTACT</a></li>	
+						
+				</ul>
+				
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="login.php"><span class="glyphicon glyphicon-log-in"> LOGIN</span></a></li>
+							<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"> LOGOUT</span></a></li>
+							<li><a href="registration.php"><span class="glyphicon glyphicon-user"> SIGN_UP</span></a></li>
+						</ul>
+					
+					
+				
+			</div>
+		</nav>
+</body>
+</html>
