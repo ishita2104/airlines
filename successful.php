0563c28fd8778d568819ae63101073b9240cf140
ishita2104 <60072783+ishita2104@users.noreply.github.com>
2021-01-16 20:43:01 +0530

    Create successful.php

---
+++ successful.php
@@ -0,0 +1,37 @@
+<?php
+	include "connection.php";
+	include "navbar.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		HISTORY PAGE
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+	<style type="text/css">
+		section
+		{
+			margin: -20px;
+			 height:580px;
+    		width: 1370px; 
+		}
+	</style>
+</head>
+
+<body>
+	<section>
+		<br><br>
+		<center><h1>TICKETS SUCCESSFULLY BOOKED !!</h1></center>
+		<center><h2>HAVE A HAPPY JOURNEY !!</h2></center>
+	</section>
+</body>
+</html>
